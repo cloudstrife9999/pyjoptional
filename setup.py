3c63fde3b2d6cd89dc48792a0cbeabf661be10d3
cloudstrife9999 <emanuele.uliana.90@gmail.com>
2023-06-01 18:00:48 +0100

    Dropped support for python 3.8.

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ author: str = "Emanuele Uliana"
 license: str = "GNU3"
 classifiers: list[str] = [
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3 :: Only",
     "Development Status :: 4 - Beta",
     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
